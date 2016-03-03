8accfea330600e398dece61f9bcbb97aeb8f9362
Igor Zinken <igor.zinken@igorski.nl>
2016-03-03 22:42:38 +0100

    added definitions file for colours, implemented unit tests for declaration of repeated patterns

--- test/factory/AssemblerFactory.test.js
+++ test/factory/AssemblerFactory.test.js
@@ -5,7 +5,9 @@ var chai             = require( "chai" );
 var MockBrowser      = require( "mock-browser" ).mocks.MockBrowser;
 var AssemblerFactory = require( "../../src/js/factory/AssemblerFactory" );
 var SongModel        = require( "../../src/js/model/SongModel" );
+var TIA              = require( "../../src/js/definitions/TIA" );
 var Time             = require( "../../src/js/utils/Time" );
+var ObjectUtil       = require( "../../src/js/utils/ObjectUtil" );
 
 describe( "AssemblerFactory", function()
 {
@@ -82,7 +84,7 @@ describe( "AssemblerFactory", function()
         var pattern = song.hats.pattern;
 
         for ( var i = 0; i < pattern.length; ++i )
-            pattern[ i ] = ( Math.random() > .5 ) ? 1 : 0;
+            pattern[ i ] = ( randBool() ) ? 1 : 0;
 
         var asm = textToLineArray( AssemblerFactory.assemblify( song ));
         var lineStart = getLineNumForText( asm, "hatPattern" ) + 1;
@@ -121,9 +123,136 @@ describe( "AssemblerFactory", function()
         assert.ok( asm[ getLineNumForText( asm, "HATSOUND equ" )].indexOf( hats.sound ) > -1,
             "expected hat sound to have been translated correctly" );
     });
+
+    it( "should declare silent patterns only once to save space", function()
+    {
+        var channel1 = song.patterns[ 0 ].channels[ 0 ];
+        var bank     = TIA.table.tunings[ 0 ].BASS;
+        var i, l, def;
+
+        // add some random notes for the first quaver of the first channels bar
+
+        for ( i = 0, l = song.patterns[0].steps / 4; i < l; ++i )
+        {
+            def = bank[ rand( 0, bank.length - 1 )];
+            channel1[ i ] = {
+                sound: "BASS",
+                note: def.note,
+                octave: def.octave,
+                accent: randBool()
+            };
+        }
+
+        var asm = textToLineArray( AssemblerFactory.assemblify( song ));
+        var patternDef = getLineNumForText( asm, "Higher volume patterns" ) + 3;
+
+        assert.ok( asm[ patternDef ].indexOf( "word Pattern1, Pattern2, Pattern2, Pattern2" ) > -1,
+            "expected one unique and three re-used patterns for channel 1" );
+
+        assert.ok( asm[ patternDef + 1 ].indexOf( "word Pattern2, Pattern2, Pattern2, Pattern2" ) > -1,
+            "expected four re-used patterns for channel 2" );
+    });
+
+    it( "should define duplicate patterns only once to save space", function()
+    {
+        var channel1 = song.patterns[ 0 ].channels[ 0 ];
+        var channel2 = song.patterns[ 0 ].channels[ 1 ];
+        var bank     = TIA.table.tunings[ 0 ].BASS;
+        var i, j, l, def;
+
+        // add some random notes for the first quaver of the first channels bar
+
+        for ( i = 0, l = song.patterns[0].steps / 4; i < l; ++i )
+        {
+            def = bank[ rand( 0, bank.length - 1 )];
+            channel1[ i ] = {
+                sound: "BASS",
+                note: def.note,
+                octave: def.octave,
+                accent: randBool()
+            };
+        }
+
+        // copy the 1st quaver defined in channel 1 into channel 2 at the start of its 2nd quaver
+
+        for ( i = 0, j = 4; i < 4; ++i, ++j )
+            channel2[ j ] = ObjectUtil.clone( channel1[ i ]);
+
+        var asm = textToLineArray( AssemblerFactory.assemblify( song ));
+        var patternDef = getLineNumForText( asm, "Higher volume patterns" ) + 3;
+
+        assert.ok( asm[ patternDef ].indexOf( "word Pattern1, Pattern2, Pattern2, Pattern2" ) > -1,
+            "expected one unique and three re-used patterns for channel 1" );
+
+        assert.ok( asm[ patternDef + 1 ].indexOf( "word Pattern2, Pattern1, Pattern2, Pattern2" ) > -1,
+            "expected three re-used silent and one re-used pattern for channel 2" );
+
+        // add some random notes for the last quaver of the second channels bar
+
+        for ( i = 12, l = 16; i < l; ++i )
+        {
+            def = bank[ rand( 0, bank.length - 1 )];
+            channel2[ i ] = {
+                sound: "BASS",
+                note: def.note,
+                octave: def.octave,
+                accent: randBool()
+            };
+        }
+        asm = textToLineArray( AssemblerFactory.assemblify( song ));
+        patternDef = getLineNumForText( asm, "Higher volume patterns" ) + 3;
+
+        assert.ok( asm[ patternDef ].indexOf( "word Pattern1, Pattern2, Pattern2, Pattern2" ) > -1,
+            "expected one unique and three re-used patterns for channel 1" );
+
+        assert.ok( asm[ patternDef + 1 ].indexOf( "word Pattern2, Pattern1, Pattern2, Pattern3" ) > -1,
+            "expected two re-used silent, one re-used pattern and one unique pattern for channel 2" );
+    });
+
+    it( "should redeclare a pattern that is a duplicate by notes, but not by accents", function()
+    {
+        var channel1 = song.patterns[ 0 ].channels[ 0 ];
+        var channel2 = song.patterns[ 0 ].channels[ 1 ];
+        var bank     = TIA.table.tunings[ 0 ].BASS;
+        var i, j, l, def;
+
+        // add some random notes for the first quaver of the first channels bar
+
+        for ( i = 0, l = song.patterns[0].steps / 4; i < l; ++i )
+        {
+          def = bank[ rand( 0, bank.length - 1 )];
+          channel1[ i ] = {
+              sound: "BASS",
+              note: def.note,
+              octave: def.octave,
+              accent: randBool()
+          };
+        }
+
+        // copy the 1st quaver defined in channel 1 into channel 2 at the start of its 2nd quaver
+
+        for ( i = 0, j = 4; i < 4; ++i, ++j ) {
+            channel2[ j ] = ObjectUtil.clone( channel1[ i ] );
+            channel2[ j ].accent = !channel1[ i ].accent; // flip the accent
+        }
+
+        var asm = textToLineArray( AssemblerFactory.assemblify( song ));
+        var patternDef = getLineNumForText( asm, "Higher volume patterns" ) + 3;
+
+        assert.ok( asm[ patternDef ].indexOf( "word Pattern1, Pattern2, Pattern2, Pattern2" ) > -1,
+            "expected one unique and three re-used patterns for channel 1" );
+
+        assert.ok( asm[ patternDef + 1 ].indexOf( "word Pattern2, Pattern3, Pattern2, Pattern2" ) > -1,
+            "expected two re-used silent, one re-used pattern and one unique pattern (different by accents) for channel 2" );
+    });
 });
 
 /* helper functions */
+
+function randBool()
+{
+    return Math.random() > .5;
+}
 
 function rand( min, max )
 {
