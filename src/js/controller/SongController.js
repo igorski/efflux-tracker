da7d827d34f2173f0b5a889dce138ac91cdf786b
Igor Zinken <igor.zinken@igorski.nl>
2016-03-12 17:08:00 +0100

    fixed bug where song exporting was broken on Firefox, the vulpine bastard

--- src/js/controller/SongController.js
+++ src/js/controller/SongController.js
@@ -149,7 +149,15 @@ function handleExport( aEvent )
         var pom = document.createElement( "a" );
         pom.setAttribute( "href", "data:text/plain;charset=utf-8," + encodeURIComponent( asm ));
         pom.setAttribute( "download", "song.h" );
-        pom.click();
+
+        if ( document.createEvent ) {
+            var event = document.createEvent( "MouseEvents" );
+            event.initEvent( "click", true, true );
+            pom.dispatchEvent( event );
+        }
+        else {
+            pom.click();
+        }
     }
 }
 
