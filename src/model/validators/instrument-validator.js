a9e77be7182f2e240ad03bb1b497b1f14b3d2b9d
Igor Zinken <igor.zinken@igorski.nl>
2021-03-16 11:28:07 +0100

    Updated Instrument fixtures to match changing in Instrument model

--- src/model/validators/instrument-validator.js
+++ src/model/validators/instrument-validator.js
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Igor Zinken 2016 - https://www.igorski.nl
+ * Igor Zinken 2016-2021 - https://www.igorski.nl
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -31,11 +31,10 @@ export default
      * @param {INSTRUMENT} instrument
      * @return {boolean}
      */
-    isValid( instrument )
-    {
-        if ( !instrument )
+    isValid( instrument ) {
+        if ( !instrument ) {
             return false;
-
+        }
         if ( !Array.isArray( instrument.oscillators )) {
             return false;
         }
