b8864519cdc47f47bf1593181e422c4b8bcd895a
Igor Zinken <igor.zinken@igorski.nl>
2023-07-06 20:59:35 +0200

    Fixtures generator now recursively creates output folder

--- fixtures/generate.js
+++ fixtures/generate.js
@@ -5,15 +5,19 @@ const outputDir     = `${process.cwd()}/public/fixtures`; // invoke from reposit
 const instrumentDir = `${currentDir}/instruments`;
 const songDir       = `${currentDir}/songs`;
 
-function mergeFiles( directory, outputFilename ) {
-    const jsonArray = fs.readdirSync( directory ).reduce(( acc, file ) => {
-        const data = fs.readFileSync( `${directory}/${file}`, "utf8" );
+function mergeFiles( inputDir, outputFilename ) {
+    const jsonArray = fs.readdirSync( inputDir ).reduce(( acc, file ) => {
+        const data = fs.readFileSync( `${inputDir}/${file}`, "utf8" );
         acc.push( JSON.parse( data ));
         return acc;
     }, []);
 
-    fs.writeFileSync( outputFilename, JSON.stringify( jsonArray ));
+    if ( !fs.existsSync( outputDir )) {
+        fs.mkdirSync( outputDir, { recursive: true });
+    }
+
+    fs.writeFileSync( `${outputDir}/${outputFilename}`, JSON.stringify( jsonArray ));
 }
 
-mergeFiles( instrumentDir, `${outputDir}/fixtures-instruments.json` );
-mergeFiles( songDir, `${outputDir}/fixtures-songs.json` );
+mergeFiles( instrumentDir, "fixtures-instruments.json" );
+mergeFiles( songDir, "fixtures-songs.json" );
