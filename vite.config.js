668df6eb2cccff3c18e9538b909e2d12e90dfd0c
Igor Zinken <730069+igorski@users.noreply.github.com>
2026-08-19 17:46:55 +0200

    And again

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import { viteStaticCopy } from "vite-plugin-static-copy";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
-const dirSrc      = `./src`;
-const dirPublic   = normalizePath( path.resolve( __dirname, "public" ));
-const dirAssets   = normalizePath( path.resolve( __dirname, "src/assets" ));
-const dest        = ".";
+const dirSrc    = `./src`;
+const dirPublic = normalizePath( path.resolve( __dirname, "public" ));
+const dirAssets = normalizePath( path.resolve( __dirname, "src/assets" ));
 
 // https://vitejs.dev/config/
 export default defineConfig({
